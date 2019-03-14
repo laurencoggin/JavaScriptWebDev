32ad8a05674c7139c8aef96cd9fcb3a9de84b35a
Lauren Coggin <46908476+laurencoggin@users.noreply.github.com>
2019-03-14 10:09:15 -0500

    Update myScripts.js

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,3 +1,5 @@
-var name = window.prompt("Please enter your name:");
-window.alert("Hello, " + name + "! How are you today?");
-console.log("Hello, " + name + "! How are you today?");
+var userName = window.prompt('Please enter your name:','default');
+
+window.alert('Hello, '+ userName);
+
+console.log('Hello, '+ userName);
